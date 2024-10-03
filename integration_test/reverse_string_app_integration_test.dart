e796652c1ed42f25f4eba67da652ca980c1e3a26
elsayedelhoseny <sayedelhoseny09@gmail.com>
2024-10-03 15:23:50 +0300

    IntegrationTest

--- integration_test/reverse_string_app_integration_test.dart
+++ integration_test/reverse_string_app_integration_test.dart
@@ -4,26 +4,34 @@ import 'package:testing/main.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // دي خطوة بنقول فيها للتطبيق يجهز نفسه عشان نعمل عليه اختبار شامل
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  // بنعمل مجموعة اختبار للتطبيق اللي بيقلب النصوص
   group('Reverse String App Test', () {
+    // هنا بنعمل اختبار لاسكرينه معينة وهي قلب النص اللي بندخله
     testWidgets('Reverse a given string', (WidgetTester tester) async {
-      // تحميل التطبيق
+      // عشان نظهر التطبيق ف بيئه العمل  pumpWidget من خلال ال widgetsTester بنحط التطبيق اللي عايزين نختبره باستخدام
       await tester.pumpWidget(const ReverseStringApp());
 
-      // البحث عن TextField واختبار كتابه نص فيه
+      // بندور على خانة الكتابة اللي المستخدم هيكتب فيها النص
       final textField = find.byType(TextField);
+
+      // Hello بنكتب ف خانه الكتابه كاننا المستخدم كلمه
       await tester.enterText(textField, 'Hello');
 
-      // البحث عن الزرار والضغط عليه
+      //  Reverse بندور على الزر اللي مكتوب عليه
       final reverseButton = find.text('Reverse');
+
+      // بنضغط علي الزرار كاننا بنجرب التطبيق بجد
       await tester.tap(reverseButton);
 
-      // تحديث واجهة المستخدم بعد الضغط
+      // بنستنى شوية لغاية ما التطبيق يخلص كل اللي بيعمله بعد الضغط
       await tester.pumpAndSettle();
 
-      // التحقق من أن النص المعكوس ظهر على الشاشة
+      // olleH بنتأكد إن النص اللي ظهر بعد الضغط على الزر هو كلمه
       expect(find.text('olleH'), findsOneWidget);
+      // وبس كدا
     });
   });
 }
